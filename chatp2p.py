737302130d59d7ccff882c6ccdb8b661b1ebf37e
Camille SAURY <Camille.Saury.Etu@univ-lemans.fr>
2018-10-31 17:43:51 +0100

    Update chatp2p.py

--- chatp2p.py
+++ chatp2p.py
@@ -4,16 +4,16 @@
 # port 1664
 ##
 from socket import *
-from select import select
+from select import *
 from sys import argv
-
+from sys import stdin
 
 socks = []
 name = ""
 s = None
 def createProfile():
 	print("what's your name ?")
-	name = raw_input()
+	name = input()
 
 def isCorrectIp(ip):
 	print(ip)
@@ -34,14 +34,16 @@ def quit():
 	print("ok")
 
 def sendMsg(dest,msg):
-	for i in socks:
-		if i == argv[1]: #il y a une socket qui pointe vers l'ip destination
-			
-		else:
-	print("ok")
+        for i in socks:
+                if i == argv[1]: #il y a une socket qui pointe vers l'ip destination
+                        print("")
+                else:
+                        print("")
+        print("ok")
 
 def sendMsgBroadcast(msg):
-
+	for s in socks:
+		sendMsg(s,msg)
 	print("ok")
 
 def init():
@@ -55,8 +57,17 @@ def init():
 def start():
 	createProfile()
 	init()
-
+	data = ""
 	while 1:
+		if data == "exit":
+			break
+		lin, lout, lex =select([stdin],[],[])
+		for s in lin:
+			if s==stdin :
+				data = stdin.readline().strip("\n")
+				print ("entrée clavier : %s" % data)
+
+
 		entry = raw_input()
 		arg = entry.split(" ")
 		nb = len(arg)
@@ -75,3 +86,4 @@ def start():
 
 
 start()
+

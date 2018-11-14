e1db3d918181c4aaffd15462ffabb969a1dc1f96
Drakefly <csenat15@outlook.com>
2018-11-14 20:31:10 +0100

    Gestion des messages reçus.

--- chatp2p.py
+++ chatp2p.py
@@ -24,6 +24,41 @@ class User: #associe une ip a un nickname
 	def __str__(self):
 		return("(" +self.name + "@" + self.addr + ")")	
 
+
+	def estBanni(c)
+		for x in self.ban :
+			if x == c :
+				return 1
+		return 0
+
+
+	def afficher() :
+
+
+	def generateSocketClient():
+
+
+	def handle(mdg,sc):#Traite et gere les string qui sont recus
+		str = x.split("\001")
+		if str[0] < 1000 and str[0] < 2000 :#todo peut mieux faire start
+			sc = generateSocketClient()
+			sendMsg(self,sc, str[1] ,2)
+			sendMsg(self,sc," ",3)
+
+		if str[0] < 2000 and str[0] < 3000 :#hello
+		#bah cool
+
+		if str[0] < 3000 and str[0] < 4000 :# ips
+		for ip in x[1].split(","):
+			users.append(ip) #On rajoute les ip 
+		if str[0] < 4000 and str[0] < 5000 :# pm
+			if estBanni(sc.getsockeName()) == 0 :
+				afficher(str[1])
+		#check ban
+		if str[0] < 5000 and str[0] < 6000 :# bm
+			#todo
+		#check ban
+
 	def sendMsg(self,socket,msg,type):#Le 1229 c'est pas des erreurs destress
 		#Je ne suis pas sur de la syntaxe du send
 		if type == 1 :
@@ -57,10 +92,12 @@ class User: #associe une ip a un nickname
 	 		sendMsgBroadcast(msg.encode('utf-8'))
 
 
+
 def createProfile():
 	print("what's your name ?")
 	name = raw_input()
 	print("hello "+name)
+
 
 def isCorrectIp(ip):
 	print(ip)
@@ -71,12 +108,14 @@ def isCorrectIp(ip):
 		quit()
 		return False
 
+
 def ban(user,ban):
 	if(user in ban):
 		print("deja blackliste")
 	else:
 		ban.append(user)
 		print (user.nickname +" avec l'ip "+user.addr+" a bien ete blackliste" )
+
 
 def unban(user,ban):
 	if(user in ban):
@@ -86,13 +125,12 @@ def unban(user,ban):
 		print("n'est pas bloque")
 		
 
-
 def createServ():
 	serv.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 	serv.bind(('0.0.0.0',1664))
 	serv.listen(5)
-	
 
+	
 def quit():
 	#quitte le serveur
 	exit()
@@ -103,12 +141,13 @@ def sendMsgBroadcast(msg):
 		sendMsg(s,msg)
 	print("ok")
 
+
 def init():
 	#creer socket qui ecoute
 	createServ()
 	if len(argv) == 2:
 		print("ip"+argv[0])
-		createServ()#ppour l'ecoute
+		createServ()#pour l'ecoute
 		#create socket pour client on l'appelle s
 		s.connect((argv[0], 1664))
 		s.send("HELLO")
@@ -118,8 +157,7 @@ def init():
 	else:
 		print("Nombres d'arguments incorects")
 		quit();
-		
-		
+
 
 def start():
 	createProfile()
@@ -138,8 +176,9 @@ def start():
 				print ("entree clavier : %s" % data)
 		for s in lout:
 			print("test")
-			d = s.recv(1024).decode("ascii")
+			d = s.recv(1024).decode(utf-8)
 			print(d)
+			handle(d,s)
 
 		if data == "quit":
 			quit()
@@ -153,5 +192,6 @@ def start():
 			print("unban")
 		else:
 			print("la commande n'est pas reconnue")
+
 
 start()

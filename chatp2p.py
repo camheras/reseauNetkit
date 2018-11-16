66c4232b3a887ee3b579044e48a71c68d72e5d13
Drakefly <csenat15@outlook.com>
2018-11-16 16:45:09 +0100

    Yoooo

--- chatp2p.py
+++ chatp2p.py
@@ -12,85 +12,82 @@ socks = []  # Pourquoi ?
 name = ""
 s = None
 serv = socket()
+nickname = name
+addr = ""
+users = []
+ban = []
+
+def estBanni(c):
+    for x in ban:
+        if x == c:
+            return 1
+    return 0
+
+def afficher(self):
+    print()
+
+def generateSocketClient(self):
+    print()
 
 
-class User:  # associe une ip a un nickname
-    def __init__(self, addr, name):
-        self.nickname = name
-        self.addr = addr[0]
-        self.users = []  # Liste des User connus
-        self.ban = []  # Les bannis, on verifie ici avant de recevoir
+def handle(self, msg, sc):  # Traite et gere les string qui sont recus
+    str = msg.split("\001")
+    if str[0] < 1000 and str[0] < 2000:  # todo peut mieux faire start
+        sc = self.generateSocketClient(self)
+        self.sendMsg(self, sc, str[1], 2)
+        self.sendMsg(self, sc, " ", 3)
 
-    def __str__(self):
-        return ("(" + self.name + "@" + self.addr + ")")
+    if str[0] < 2000 and str[0] < 3000:  # hello
+        print ()# bah cool
 
-    def estBanni(c):
-        for x in ban:
-            if x == c:
-                return 1
-        return 0
-
-    def afficher(self):
-        print()
-
-    def generateSocketClient(self):
-        print()
-
-
-    def handle(self, msg, sc):  # Traite et gere les string qui sont recus
-        str = msg.split("\001")
-        if str[0] < 1000 and str[0] < 2000:  # todo peut mieux faire start
-            sc = self.generateSocketClient(self)
-            self.sendMsg(self, sc, str[1], 2)
-            self.sendMsg(self, sc, " ", 3)
-
-        if str[0] < 2000 and str[0] < 3000:  # hello
-            print ()# bah cool
-
-        if str[0] < 3000 and str[0] < 4000:  # ips
-            for ip in msg[1].split(","):
-                self.users.append(ip)  # On rajoute les ip
-        if str[0] < 4000 and str[0] < 5000:  # pm
-            if self.estBanni(sc.getsockeName()) == 0:
-                self.afficher(str[1])
-        # check ban
-        if str[0] < 5000 and str[0] < 6000:  # bm
-            print("Un message est arrivé en broadcast")
-
-    # todo
+    if str[0] < 3000 and str[0] < 4000:  # ips
+        for ip in msg[1].split(","):
+            self.users.append(ip)  # On rajoute les ip
+    if str[0] < 4000 and str[0] < 5000:  # pm
+        if self.estBanni(sc.getsockeName()) == 0:
+            print(str[1])
     # check ban
+    if str[0] < 5000 and str[0] < 6000:  # bm
+        print("Un message est arrive en broadcast")
 
-    def sendMsg(self, socket, msg, type):  # Le 1229 c'est pas des erreurs destress
-        # Je ne suis pas sur de la syntaxe du send
-        if type == 1:
-            message = ""
-            message = 1229 + "\001" + self.nickname  # SEND START
-            buf = message.encode('utf-8')
-            socket.send(buf)
+# todo
+# check ban
 
-        if type == 2:  # send nickname de l'interlocuteur, il faurait avoir mis dans le msg le nickname du receptioniste
-            message = ""
-            message = 2229 + "\001" + msg
-            buf = message.encode('utf-8')
-            socket.send(buf)
+def sendMsg(socket, msg, type):  # Le 1229 c'est pas des erreurs destress
+    # Je ne suis pas sur de la syntaxe du send
+    if type == 1:
+        message = ""
+        message = 1229 + "\001" + nickname  # SEND START
+        print("send message :" + message)
+        buf = message.encode('utf-8')
+        socket.send(buf)
 
-        if type == 3:  # TODO
-            ips = ""
-            for x in self.users :
-                ips = x.addr + " ," + ips
+    if type == 2:  # send nickname de l'interlocuteur, il faurait avoir mis dans le msg le nickname du receptioniste
+        message = ""
+        message = 2229 + "\001" + msg
+        print("send message :" + message)
+        buf = message.encode('utf-8')
+        socket.send(buf)
 
-            message = (3229 + "\001" + ips)
-            buf = message.encode('utf-8')
-            socket.send(buf)
+    if type == 3:  # TODO
+        ips = ""
+        for x in users :
+            ips = x.addr + " ," + ips
 
-        if type == 4:  # Send PM
-            message = ""
-            message = 4229 + "\001" + msg
-            buf = message.encode('utf-8')
-            socket.send(buf)
+        message = (3229 + "\001" + ips)
+        print("send message :" + message)
+        buf = message.encode('utf-8')
+        socket.send(buf)
 
-        if type == 5:  # TODO ca ne marcheras pas
-            sendMsgBroadcast(msg.encode('utf-8'))
+    if type == 4:  # Send PM
+        message = ""
+        message = 4229 + "\001" + msg
+        print("send message :" + message)
+        buf = message.encode('utf-8')
+        socket.send(buf)
+
+    if type == 5:  # TODO ca ne marcheras pas
+        sendMsgBroadcast(msg.encode('utf-8'))
 
 
 def createProfile():
@@ -145,16 +142,17 @@ def sendMsgBroadcast(msg):
 
 def init():
 	#creer socket qui ecoute
-	createServ()
-	if len(argv) == 2:
+    createServ()
+    if len(argv) == 2:
 		print("ip "+argv[1])
 		#create socket pour client on l'appelle s
 		s = socket(AF_INET, SOCK_STREAM)
-		s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)		
+		s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		s.connect((argv[1], 1664))
 		print(s.getsockname())
 		socks.append(s)
 		s.send("START")
+
 
 def msg(data):
 	if data == "quit":
@@ -169,31 +167,31 @@ def msg(data):
 		print("unban")
 	else:
 		print("la commande n'est pas reconnue")
-		
-
+        
+        
 def start():
-	createProfile()
-	init()
-	data = ""
-	
-	socks.append(stdin)
+    createProfile()
+    init()
+    data = ""
 
-	while 1:
-		if data == "exit":
-			break
-			
-		lin, lout, lex =select(socks,[],[])
-		
-		for s in lin:
-			if s==stdin:
-				data = stdin.readline().strip("\n")
-				print ("entree clavier : %s" % data)
-				msg(data)
-			if s==serv:
-				print("test")
-				sc, addr = s.accept()
-				d = sc.recv(1024).decode("ascii")
-				print(d)
+    socks.append(stdin)
+
+    while 1:
+        if data == "exit":
+            break
+
+        lin, lout, lex = select(socks, [], [])
+
+        for s in lin:
+            if s == stdin:
+                data = stdin.readline().strip("\n")
+                print ("entree clavier : %s" % data)
+                msg(data)
+            if s == serv:
+                print("test")
+                sc, addr = s.accept()
+                d = sc.recv(1024).decode("ascii")
+                print(d)
 
 
 start()
